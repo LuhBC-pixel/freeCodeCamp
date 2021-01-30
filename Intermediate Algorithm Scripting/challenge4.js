9f0b7d6e70d1ac5245a60fada8e105a2d9caa3cd
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-01-30 13:35:01 -0300

    Resolve challenge 4

---
+++ Intermediate Algorithm Scripting/challenge4.js
@@ -0,0 +1,14 @@
+function whatIsInAName(collection, source) {
+    const property = Object.keys(source);
+    const arrObj = [...collection];
+
+    const arr = arrObj.filter(elem => {
+        return property.every(key => elem.hasOwnProperty(key) && elem[key] === source[key]);
+    });
+
+    return arr;
+}
+
+whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, { first: "Tybalt", last: "Capulet" }], { last: "Capulet" });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "bat": 2 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "bat": 2 });
+whatIsInAName([{ "apple": 1, "bat": 2 }, { "apple": 1 }, { "apple": 1, "bat": 2, "cookie": 2 }], { "apple": 1, "cookie": 2 })
