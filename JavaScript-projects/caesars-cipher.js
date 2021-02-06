8a4d92916e5d8a2458e7ed88df484f1127f630ba
LuhBC-pixel <bc.luisa@yahoo.com.br>
2021-02-06 18:04:31 -0300

    resolve caesars cipher

---
+++ JavaScript-projects/caesars-cipher.js
@@ -0,0 +1,24 @@
+function rot13(str) {
+    const arr = str.split('');
+
+    let stringDecode = '';
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].charCodeAt() >= 65 && arr[i].charCodeAt() <= 90) {
+            if (arr[i].charCodeAt() > 77) {
+                stringDecode += String.fromCharCode(arr[i].charCodeAt() - 13);
+            } else {
+                stringDecode += String.fromCharCode(arr[i].charCodeAt() + 13);
+            }
+        } else {
+            stringDecode += arr[i];
+        }
+    }
+
+    return stringDecode;
+}
+
+rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT.")
